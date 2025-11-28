54de7419954ed6c74a844b9cfc4c6f38286b2870
Priscilla Abhulimen <priscillaabhulimen@gmail.com>
2025-11-28 13:46:36 -0400

    l2 complete

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: "Socks"
+            product: "Socks",
+            description: "Warm and fuzzy for keeping your feet warmon those chilly days"
         }
     }
 })
